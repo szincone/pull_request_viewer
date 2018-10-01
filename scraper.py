df0aade565a6598d6535e4d5d221d37eebd6924f
szincone <sawyerzincone@gmail.com>
2018-09-30 23:45:39 -0600

    updated comments and used new string interpolation method

--- scraper.py
+++ scraper.py
@@ -6,9 +6,10 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-handles = os.getenv("HANDLES_LIST")
-url = os.getenv("URL")
+handles = os.getenv("HANDLES_LIST") # list of usernames ['Fry', 'Bender']
+url = os.getenv("URL") # string containing url "github.com/blah/pulls" to be scraped
 
+# selenium
 browser = webdriver.Firefox()
 browser.get(url)
 
@@ -16,13 +17,7 @@ def find_user_links(handles_list):
 
 	for user in handles_list:
 		# print(browser.find_element_by_css_selector('.muted-link[title="Open pull request created by str(user.replace('"', ''))"]'))
-		print(browser.find_element_by_css_selector(".muted-link"))
+		print(browser.find_element_by_css_selector(f'.muted-link['))
 
 
 find_user_links(handles)
-
-# assign global variable for each user in handlers array
-# for user in handles:
-# 	i = 0
-# 	globals()[user] = handlers[i]
-# 	i += 1;
