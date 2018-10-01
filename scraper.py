598702284e594096cdb4a2eaf1dfa261ffe9a601
szincone <sawyerzincone@gmail.com>
2018-10-01 01:36:25 -0600

    converted handles string to list, correctly locating userlinks

--- scraper.py
+++ scraper.py
@@ -1,23 +1,26 @@
 #! python 3
-# scraper.py - opens multiple github pull-requests in new browser depending on usernames
+# scraper.py - opens multiple github pull-requests in new browser
+# depending on usernames
 from selenium import webdriver
 # gets us access to our environment_vars (os needed)
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-handles = os.getenv("HANDLES_LIST") # list of usernames ['Fry', 'Bender']
-url = os.getenv("URL") # string containing url "github.com/blah/pulls" to be scraped
+# string of usernames "Fry Bender Leela" turned into list
+handles = os.getenv("HANDLES_LIST").split(" ")
+# string containing url "github.com/blah/pulls" to be scraped
+url = os.getenv("URL")
 
 # selenium
 browser = webdriver.Firefox()
 browser.get(url)
 
-def find_user_links(handles_list):
 
-	for user in handles_list:
-		# print(browser.find_element_by_css_selector('.muted-link[title="Open pull request created by str(user.replace('"', ''))"]'))
-		print(browser.find_element_by_css_selector(f'.muted-link['))
+def find_user_links(handles_list):
+    ###
+    for user in handles_list:
+        print(browser.find_element_by_link_text(user))
 
 
 find_user_links(handles)
